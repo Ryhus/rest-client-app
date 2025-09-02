f73b85f6ca69b04d123866d854e048129084a606
Ryhus <ryhusyevhen@gmail.com>
2025-09-02 13:36:16 +0200

    feat(auth elements): create basic auth components
    - create supabase client
    - create basic forms and input components
    - create login and signup pages

---
+++ src/components/Forms/SignUpForm.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import { Form } from 'react-router-dom';
+import FormInput from './FormInput/FormInput';
+
+import './FormStyles.scss';
+
+export default function SignUpForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Form className="form" method="post">
+      <FormInput
+        id="email"
+        name="email"
+        labelText="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      ></FormInput>
+      <FormInput
+        id="password"
+        name="password"
+        labelText="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      ></FormInput>
+      <button type="submit" className="submit-form-bttn">
+        Submit
+      </button>
+    </Form>
+  );
+}
---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,12 @@
+import './SignInStyles.scss';
+
+import { SignInForm } from '@/components';
+
+export default function SignIn() {
+  return (
+    <div className="signup-page">
+      <h2>Login</h2>
+      <SignInForm />
+    </div>
+  );
+}
