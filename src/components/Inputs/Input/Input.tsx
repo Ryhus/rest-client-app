b176db4c1f7ca31448dcdfbde3384e55f4a21591
anakdrch <ana.kudrevich@gmail.com>
2025-09-04 20:23:27 +0200

    feat: implement Input component

---
+++ src/components/Inputs/Input/Input.tsx
@@ -0,0 +1,74 @@
+import './InputStyles.scss';
+
+interface InputError {
+  id: number;
+  message: string;
+}
+
+interface InputProps {
+  id: string;
+  type?: string;
+  name?: string;
+  value?: string;
+  checked?: boolean;
+  labelText?: string;
+  className?: string;
+  list?: string;
+  errors?: InputError[];
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isDisabled?: boolean;
+}
+
+function Input({
+  name,
+  value,
+  checked,
+  id,
+  type = 'text',
+  labelText,
+  list,
+  className = '',
+  errors,
+  rightIcon,
+  onChange,
+  isDisabled,
+}: InputProps) {
+  const inputContainerClass =
+    `input-container input-container__${type}  ${className}`.trim();
+
+  const inputFieldClass =
+    `input-field input-field__${type} ${rightIcon ? 'input-field--right-icon' : ''}`.trim();
+
+  return (
+    <div className={inputContainerClass}>
+      <label className="input-label" htmlFor={id}>
+        {labelText}
+      </label>
+      <div className="input-wrapper">
+        <input
+          name={name}
+          id={id}
+          type={type}
+          value={value}
+          checked={checked}
+          list={list}
+          className={inputFieldClass}
+          onChange={onChange}
+          disabled={isDisabled}
+        ></input>
+        {rightIcon && (
+          <div className="input-icon input-right-icon">{rightIcon}</div>
+        )}
+        <div className="input-field--error">
+          {errors?.map((error) => (
+            <div key={error.id}>{error.message}</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
