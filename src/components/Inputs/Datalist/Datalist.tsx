07ebeea126c71a79234c44d678c2f119cce1b738
anakdrch <ana.kudrevich@gmail.com>
2025-09-04 15:22:52 +0200

    feat: implement Datalist component

---
+++ src/components/Inputs/Datalist/Datalist.tsx
@@ -0,0 +1,45 @@
+import type { ChangeEventHandler } from 'react';
+import './DatalistStyles.scss';
+
+interface DatalistProps {
+  id: string;
+  name?: string;
+  labelTxt?: string;
+  value: string;
+  listName: string;
+  placeholder?: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  data: string[];
+}
+
+export default function Datalist({
+  id,
+  name,
+  labelTxt,
+  value,
+  listName,
+  placeholder,
+  onChange,
+  data,
+}: DatalistProps) {
+  return (
+    <div className="datalist">
+      <label htmlFor={id}>{labelTxt}</label>
+      <input
+        id={id}
+        list={listName}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <datalist id={listName}>
+        {data.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </datalist>
+    </div>
+  );
+}
