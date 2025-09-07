9f5215e30138c543f496706394a7f1c2d56803df
anakdrch <ana.kudrevich@gmail.com>
2025-09-07 19:23:41 +0200

    feat: update header to match design template

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,45 +1,54 @@
 import { Link } from 'react-router-dom';
 import { useAuthStore } from '@/stores/authStore/authStore';
 import { supabase } from '@/services/supabase';
+import logo from '@/assets/img/logo.svg';
+import { useEffect, useRef, useState } from 'react';
 
 import './HeaderStyles.scss';
 
 export default function Header() {
   const session = useAuthStore((s) => s.session);
   const loading = useAuthStore((s) => s.loading);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const headerClass = isScrolled ? 'header scrolled' : 'header';
+  const headerRef = useRef<HTMLElement>(null);
+
+  const handleScroll = () => {
+    if (headerRef.current && window.scrollY > headerRef.current.offsetHeight) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <header className="header">
-      <Link to="/" className="header__logo">
-        Rest Client
+    <header className={headerClass} ref={headerRef}>
+      <Link to="/" className="header__link">
+        <img className="header__logo" src={logo} alt="Rest client app logo" />
       </Link>
 
       {!loading && (
         <div className="header__actions">
           {session ? (
             <>
-              <Link to="/" className="header__button">
+              <Link to="/" className="header__link button secondary">
                 Home
               </Link>
-              <button
-                className="header__button header__button--signout"
-                onClick={() => supabase.auth.signOut()}
-              >
+              <button className="button primary" onClick={() => supabase.auth.signOut()}>
                 Sign Out
               </button>
             </>
           ) : (
             <>
-              <Link
-                to="/login"
-                className="header__button header__button--signin"
-              >
+              <Link to="/login" className="header__link button secondary">
                 Sign In
               </Link>
-              <Link
-                to="/signup"
-                className="header__button header__button--signup"
-              >
+              <Link to="/signup" className="header__link button secondary">
                 Sign Up
               </Link>
             </>
