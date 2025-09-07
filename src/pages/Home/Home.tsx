b368819985c5d0410d8600e403886b827b89d86a
anakdrch <ana.kudrevich@gmail.com>
2025-09-07 16:42:29 +0200

    feat: update home page to match design template

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -14,15 +14,11 @@ export default function Home() {
 
   return (
     <main className="home">
-      <h1 className="home__tittle">
-        {session
-          ? `Welcome back, ${userData?.name ?? 'Dear User'}!`
-          : 'Welcome!'}
-      </h1>
+      <h1 className="home__tittle">{session ? `Welcome back, ${userData?.name ?? 'Dear User'}!` : 'Welcome!'}</h1>
 
       <nav className="home__nav">
         {links.map((link) => (
-          <Link key={link.text} to={link.to} className="home__link">
+          <Link key={link.text} to={link.to} className="home__link button secondary">
             {link.text}
           </Link>
         ))}
