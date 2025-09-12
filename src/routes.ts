82faa0c7328a0b457000c88df9550353ee32e9fe
Ryhus <ryhusyevhen@gmail.com>
2025-09-12 11:16:13 +0200

    feat(routes): add new routes for logout and history, fix index path for SSR deployment

--- src/routes.ts
+++ src/routes.ts
@@ -2,21 +2,22 @@ import { type RouteConfig } from '@react-router/dev/routes';
 
 export default [
   {
-    path: '/',
     file: './layouts/MainLayout/MainLayout.tsx',
     children: [
-      {
-        index: true,
-        file: './pages/Home/Home.tsx',
-      },
-      {
-        path: 'signup',
-        file: './pages/SignUp/SignUp.tsx',
-      },
+      { index: true, file: './pages/Home/Home.tsx' },
+      { path: 'signup', file: './pages/SignUp/SignUp.tsx' },
       { path: 'login', file: './pages/SignIn/SignIn.tsx' },
       {
         path: '/rest-client/:method?/:encodedUrl?/:encodedBody?',
         file: './pages/RestClient/RestClient.tsx',
+      },
+      {
+        path: 'history',
+        file: './pages/History/History.tsx',
+      },
+      {
+        path: 'logout',
+        file: './pages/Logout/Logout.tsx',
       },
     ],
   },
