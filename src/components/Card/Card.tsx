51b7e56496a53f4e21e6d2e92bcea73e24f7a47e
anakdrch <ana.kudrevich@gmail.com>
2025-09-15 10:59:09 +0200

    fix: fix styles of Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -13,22 +13,23 @@ interface Profile {
 export default function Card(profile: Profile) {
   return (
     <div className="card-container">
-      <div className="card-image">
-        <img src={profile.image} alt={profile.name} />
+      <div>
+        <img className="card-image" src={profile.image} alt={profile.name} />
       </div>
       <div className="card-content">
-        <h1 className="card-title">{profile.name}</h1>
-        <p className="card-role">{profile.role}</p>
-        <p className="card-description">{profile.description}</p>
-        <a
-          className={profile.role === 'team lead' ? 'card-link-primary' : 'card-link'}
-          href={profile.gh}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={GHLogo} alt="GH Logo" />
-        </a>
+        <div className="card-header">
+          <h1 className="card-title">{profile.name}</h1>
+          <p className={profile.role === 'team lead' ? 'card-role-primary' : 'card-role'}>
+            {profile.role}
+          </p>
+        </div>
+        <div className="card-description">
+          <p>{profile.description}</p>
+        </div>
       </div>
+      <a href={profile.gh} target="_blank" rel="noopener noreferrer">
+        <img className="card-gh-logo" src={GHLogo} alt="GH Logo" />
+      </a>
     </div>
   );
 }
