8690d196ed200d7f81beef4f8f0e20854e366bac
anakdrch <ana.kudrevich@gmail.com>
2025-09-17 19:07:44 +0200

    feat: add language detector

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { resources } from './locales/resources';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n.use(initReactI18next).init({
-  debug: true,
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-  resources,
-});
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    supportedLngs: ['en', 'ru'],
+    load: 'languageOnly',
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+    resources,
+  });
 
 export default i18n;
