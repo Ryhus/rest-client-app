0c59f3e92e80125d0403650179f31cc5a1d92c70
anakdrch <ana.kudrevich@gmail.com>
2025-09-17 10:30:41 +0200

    feat: add internalization to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
 import { Link, useRouteLoaderData, Form } from 'react-router-dom';
 import logo from '@/assets/img/logo.svg';
 import { useEffect, useState } from 'react';
-import { userLinks } from '@/utils/navLinksConfig';
+import { guestLinks, userLinks } from '@/utils/navLinksConfig';
 import { type User } from '@supabase/supabase-js';
+import { useTranslation } from 'react-i18next';
+import { languages } from '@/utils/languages';
+import LangToggler from '../LangToggler/LangToggler';
+
 import './HeaderStyles.scss';
 
 export default function Header() {
   const user = useRouteLoaderData<User>('root');
   const [isScrolled, setIsScrolled] = useState(false);
   const headerClass = isScrolled ? 'header scrolled' : 'header';
+  const { t } = useTranslation('header');
 
   const handleScroll = () => {
     if (window.scrollY > 0) {
@@ -36,31 +41,28 @@ export default function Header() {
               <div className="navbar__actions--expanded-group">
                 {userLinks.map((link) => (
                   <Link key={link.text} to={link.to} className="navbar__link">
-                    {link.text}
+                    {t(link.text)}
                   </Link>
                 ))}
-                <Link to="/" className="navbar__link">
-                  Home
-                </Link>
               </div>
               <div className="navbar__actions--basic-group">
                 <Form method="post" action="/logout">
-                  <button className="button secondary">Sign Out</button>
+                  <button className="button secondary">{t('signOut')}</button>
                 </Form>
               </div>
             </>
           ) : (
             <div className="navbar__actions--basic-group">
-              <Link to="/login" className="button secondary">
-                Sign In
-              </Link>
-              <Link to="/signup" className="button secondary">
-                Sign Up
-              </Link>
+              {guestLinks.map((link) => (
+                <Link key={link.text} to={link.to} className="button secondary">
+                  {t(link.text)}
+                </Link>
+              ))}
             </div>
           )}
         </div>
       </nav>
+      <LangToggler languages={languages} />
     </header>
   );
 }
--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './i18n';
 import { HydratedRouter } from 'react-router/dom';
 
 import './styles/globals.scss';
